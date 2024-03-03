5456f71e0ccd501c19f244edbde9ec6f25a31301
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-03-03 12:00:56 +0530

    Update Home.jsx

--- Client/src/components/Dashboards/StudentDashboard/Home.jsx
+++ Client/src/components/Dashboards/StudentDashboard/Home.jsx
@@ -18,7 +18,7 @@ const List = () => {
   ]);
   useEffect(() => {
     let student = JSON.parse(localStorage.getItem("student"));
-    fetch("http://localhost:3000/api/invoice/student", {
+    fetch("hostel-management-l4jb.vercel.app/api/invoice/student", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -119,7 +119,7 @@ function Home() {
 
   const getAttendance = async () => {
     let student = JSON.parse(localStorage.getItem("student"));
-    const res = await fetch("http://localhost:3000/api/attendance/get", {
+    const res = await fetch("hostel-management-l4jb.vercel.app/api/attendance/get", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
