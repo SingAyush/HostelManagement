1d5795f7e00efe69ff3d5bf0516da9e75751a490
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-03-03 16:17:43 +0530

    Update Contact.jsx

--- Client/src/components/LandingSite/Contact/Contact.jsx
+++ Client/src/components/LandingSite/Contact/Contact.jsx
@@ -41,8 +41,7 @@ function Contact() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">If you are facing any problem or if you have any querie, Let us know.</p>
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact The Admins</h2>
         <form onSubmit={handleSubmit} className="space-y-8">
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
