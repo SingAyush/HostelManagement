b829a0c590ad5e09c919b5f219dac68f9a2e6743
Ayush Singh <119442358+SingAyush@users.noreply.github.com>
2024-03-05 21:26:03 +0530

    Update Index.jsx

--- Client/src/components/LandingSite/AuthPage/Index.jsx
+++ Client/src/components/LandingSite/AuthPage/Index.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function AuthPage() {
   return (
     <>
-      <section className="bg-gray-900">
+      <section className="bg-gray-800">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-[73vh] lg:py-0">
           <a
             href="/"
